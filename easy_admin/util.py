77706b004ad3aada99c81603bc559a73cedc9c00
suchang <1103232282@qq.com>
2019-01-22 23:29:34 +0800

    感觉可以用的handler

--- easy_admin/util.py
+++ easy_admin/util.py
@@ -7,5 +7,32 @@ def str2hump(text):
     arr = filter(None, text.lower().split('_'))
     res = ''
     for i in arr:
-        res =  res + i[0].upper() + i[1:]
+        res = res + i[0].upper() + i[1:]
     return res
+
+
+def default_url_condition(args: dict) -> (dict, dict, dict):
+    """
+    默认的url参数条件解析
+    :param args:
+    :return:
+    """
+    query = {}
+    pager = {}
+    sorter = {}
+    if args:
+        for k, v in args.items():
+            if k == '_per_page':
+                pager['_per_page'] = v
+            elif k == '_page':
+                pager['_page'] = v
+            elif k == '_order_by':
+                sorter['_order_by'] = v
+            elif k == '_desc':
+                sorter['order'] = v
+            else:
+                if isinstance(v, list):
+                    query[k] = v
+                else:
+                    query[k] = [v]
+    return query, pager, sorter
