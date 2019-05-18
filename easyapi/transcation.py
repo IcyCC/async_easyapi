bb6e72a9cfd9ea3af81cda4ee1283af58d2bb238
zhandong <1468767640@qq.com>
2019-05-19 03:11:16 +0800

    fix transaction error

--- easyapi/transcation.py
+++ easyapi/transcation.py
@@ -1,5 +1,5 @@
-
 class Transaction():
+
     def __init__(self, db):
         self._db = db
         self._transaction = None
@@ -11,13 +11,21 @@ class Transaction():
         return self._connect
 
     def __exit__(self, exc_type, exc, tb):
-        try:
-            self._transaction.commit()
-        except Exception as e:
-            self._transaction.rollback()
-            raise e
-        finally:
-            self._connect.close()
+        if exc_type is None and exc is None and tb is None:
+            try:
+                self._transaction.commit()
+            except Exception as e:
+                self._transaction.rollback()
+                raise e
+            finally:
+                self._connect.close()
+        else:
+            try:
+                self._transaction.rollback()
+            except Exception as e:
+                raise e
+            finally:
+                self._connect.close()
 
 
 def get_tx(db):
