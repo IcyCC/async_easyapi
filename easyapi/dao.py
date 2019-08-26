31ae690d10a0b21d0b29391d9f13e2cd3d343291
吴坤泽 <894607015@qq.com>
2019-08-26 21:55:28 +0800

    DaoMetaClass fix _gte_

--- easyapi/dao.py
+++ easyapi/dao.py
@@ -33,7 +33,7 @@ class DaoMetaClass(type):
 
         for c in table.c:
             attrs[c.name] = c.name
-            attrs['_lte_' + c.name] = '_lte_' + c.name
+            attrs['_gte_' + c.name] = '_gte_' + c.name
             attrs['_gt_' + c.name] = '_gt_' + c.name
 
             attrs['_lte_' + c.name] = '_lte_' + c.name
